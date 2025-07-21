7900959069e189a5733dc5f607f31aeac4836cc4
yunhong <337361684@qq.com>
2025-07-21 19:52:38 +0800

    [server] Introduce rebalance

--- fluss-flink/fluss-flink-common/src/test/java/org/apache/fluss/flink/procedure/FlinkProcedureITCase.java
+++ fluss-flink/fluss-flink-common/src/test/java/org/apache/fluss/flink/procedure/FlinkProcedureITCase.java
@@ -17,11 +17,15 @@
 
 package org.apache.fluss.flink.procedure;
 
+import org.apache.fluss.client.Connection;
+import org.apache.fluss.client.ConnectionFactory;
+import org.apache.fluss.client.admin.Admin;
 import org.apache.fluss.config.ConfigOptions;
 import org.apache.fluss.config.Configuration;
 import org.apache.fluss.config.MemorySize;
 import org.apache.fluss.exception.SecurityDisabledException;
 import org.apache.fluss.metadata.DataLakeFormat;
+import org.apache.fluss.metadata.TablePath;
 import org.apache.fluss.server.testutils.FlussClusterExtension;
 
 import org.apache.flink.table.api.EnvironmentSettings;
@@ -29,6 +33,8 @@ import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.types.Row;
 import org.apache.flink.util.CloseableIterator;
 import org.apache.flink.util.CollectionUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -42,6 +48,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 import static org.apache.fluss.flink.source.testutils.FlinkRowAssertionsUtils.assertResultsIgnoreOrder;
+import static org.apache.fluss.server.testutils.FlussClusterExtension.BUILTIN_DATABASE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -51,15 +58,28 @@ public abstract class FlinkProcedureITCase {
     @RegisterExtension
     public static final FlussClusterExtension FLUSS_CLUSTER_EXTENSION =
             FlussClusterExtension.builder()
-                    .setNumOfTabletServers(3)
+                    .setNumOfTabletServers(4)
                     .setCoordinatorServerListeners("FLUSS://localhost:0, CLIENT://localhost:0")
                     .setTabletServerListeners("FLUSS://localhost:0, CLIENT://localhost:0")
                     .setClusterConf(initConfig())
                     .build();
 
     static final String CATALOG_NAME = "testcatalog";
+    static final String DEFAULT_DB = "defaultdb";
+    static Configuration clientConf;
+    static String bootstrapServers;
+    static Connection conn;
+    static Admin admin;
 
     TableEnvironment tEnv;
+
+    @BeforeAll
+    protected static void beforeAll() {
+        clientConf = FLUSS_CLUSTER_EXTENSION.getClientConfig();
+        bootstrapServers = FLUSS_CLUSTER_EXTENSION.getBootstrapServers();
+        conn = ConnectionFactory.createConnection(clientConf);
+        admin = conn.getAdmin();
+    }
 
     @BeforeEach
     void before() throws ExecutionException, InterruptedException {
@@ -84,6 +104,14 @@ public abstract class FlinkProcedureITCase {
                         CATALOG_NAME, bootstrapServers);
         tEnv.executeSql(catalogDDL).await();
         tEnv.executeSql("use catalog " + CATALOG_NAME);
+        tEnv.executeSql("create database " + DEFAULT_DB);
+        tEnv.useDatabase(DEFAULT_DB);
+    }
+
+    @AfterEach
+    void after() {
+        tEnv.useDatabase(BUILTIN_DATABASE);
+        tEnv.executeSql(String.format("drop database %s cascade", DEFAULT_DB));
     }
 
     @Test
@@ -91,7 +119,14 @@ public abstract class FlinkProcedureITCase {
         try (CloseableIterator<Row> showProceduresIterator =
                 tEnv.executeSql("show procedures").collect()) {
             List<String> expectedShowProceduresResult =
-                    Arrays.asList("+I[sys.add_acl]", "+I[sys.drop_acl]", "+I[sys.list_acl]");
+                    Arrays.asList(
+                            "+I[sys.add_acl]",
+                            "+I[sys.drop_acl]",
+                            "+I[sys.list_acl]",
+                            "+I[sys.add_server_tag]",
+                            "+I[sys.remove_server_tag]",
+                            "+I[sys.rebalance]",
+                            "+I[sys.cancel_rebalance]");
             // make sure no more results is unread.
             assertResultsIgnoreOrder(showProceduresIterator, expectedShowProceduresResult, true);
         }
@@ -129,13 +164,11 @@ public abstract class FlinkProcedureITCase {
                                 String.format(
                                         "Call %s.sys.list_acl( resource => 'ANY')", CATALOG_NAME))
                         .collect()) {
-            List<String> acls =
-                    CollectionUtil.iteratorToList(listProceduresIterator).stream()
-                            .map(Row::toString)
-                            .collect(Collectors.toList());
-            assertThat(acls)
-                    .containsExactlyInAnyOrder(
-                            "+I[resource=\"cluster\";permission=\"ALLOW\";principal=\"User:Alice\";operation=\"READ\";host=\"*\"]");
+            assertCallResult(
+                    listProceduresIterator,
+                    new String[] {
+                        "+I[resource=\"cluster\";permission=\"ALLOW\";principal=\"User:Alice\";operation=\"READ\";host=\"*\"]"
+                    });
         }
     }
 
@@ -164,21 +197,15 @@ public abstract class FlinkProcedureITCase {
                         CATALOG_NAME);
         tEnv.executeSql(addAcl).await();
         try (CloseableIterator<Row> listProceduresIterator = tEnv.executeSql(listAcl).collect()) {
-            List<String> acls =
-                    CollectionUtil.iteratorToList(listProceduresIterator).stream()
-                            .map(Row::toString)
-                            .collect(Collectors.toList());
-            assertThat(acls)
-                    .containsExactlyInAnyOrder(
-                            "+I[resource=\"cluster\";permission=\"ALLOW\";principal=\"User:Alice\";operation=\"READ\";host=\"127.0.0.1\"]");
+            assertCallResult(
+                    listProceduresIterator,
+                    new String[] {
+                        "+I[resource=\"cluster\";permission=\"ALLOW\";principal=\"User:Alice\";operation=\"READ\";host=\"127.0.0.1\"]"
+                    });
         }
         tEnv.executeSql(dropAcl).await();
         try (CloseableIterator<Row> listProceduresIterator = tEnv.executeSql(listAcl).collect()) {
-            List<String> acls =
-                    CollectionUtil.iteratorToList(listProceduresIterator).stream()
-                            .map(Row::toString)
-                            .collect(Collectors.toList());
-            assertThat(acls).isEmpty();
+            assertCallResult(listProceduresIterator, new String[0]);
         }
     }
 
@@ -219,6 +246,78 @@ public abstract class FlinkProcedureITCase {
                                         .wait())
                 .hasMessageContaining(
                         "No enum constant org.apache.fluss.security.acl.PermissionType.UNKNOWN");
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void testAddAndRemoveServerTag(boolean upperCase) throws Exception {
+        String addServerTag =
+                String.format(
+                        upperCase
+                                ? "Call %s.sys.add_server_tag(0, 'PERMANENT_OFFLINE')"
+                                : "Call %s.sys.add_server_tag(0, 'permanent_offline')",
+                        CATALOG_NAME);
+        try (CloseableIterator<Row> listProceduresIterator =
+                tEnv.executeSql(addServerTag).collect()) {
+            assertCallResult(listProceduresIterator, new String[] {"+I[success]"});
+        }
+
+        // test add duplicated server tag.
+        assertThatThrownBy(() -> tEnv.executeSql(addServerTag).wait())
+                .hasMessageContaining("Server tag PERMANENT_OFFLINE already exists for server 0.");
+
+        String removeServerTag =
+                String.format(
+                        upperCase
+                                ? "Call %s.sys.remove_server_tag(0, 'PERMANENT_OFFLINE')"
+                                : "Call %s.sys.remove_server_tag(0, 'permanent_offline')",
+                        CATALOG_NAME);
+        try (CloseableIterator<Row> listProceduresIterator =
+                tEnv.executeSql(removeServerTag).collect()) {
+            assertCallResult(listProceduresIterator, new String[] {"+I[success]"});
+        }
+
+        // test remove non-exist server tag.
+        assertThatThrownBy(() -> tEnv.executeSql(removeServerTag).wait())
+                .hasMessageContaining("Server tag PERMANENT_OFFLINE not exists for server 0.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void testRebalance(boolean upperCase) throws Exception {
+        // first create some unbalance assignment table.
+        for (int i = 0; i < 10; i++) {
+            String tableName = "reblance_test_tab_" + i;
+            tEnv.executeSql(
+                    String.format(
+                            "create table %s (a int, b varchar, c bigint, d int ) "
+                                    + "with ('connector' = 'fluss', 'table.generate-unbalance-table-assignment'='true')",
+                            tableName));
+            long tableId =
+                    admin.getTableInfo(TablePath.of(DEFAULT_DB, tableName)).get().getTableId();
+            FLUSS_CLUSTER_EXTENSION.waitUntilTableReady(tableId);
+        }
+
+        String rebalance =
+                String.format(
+                        upperCase
+                                ? "Call %s.sys.rebalance('REPLICA_DISTRIBUTION_GOAL;LEADER_REPLICA_DISTRIBUTION_GOAL', true)"
+                                : "Call %s.sys.rebalance('replica_distribution_goal;leader_replica_distribution_goal', true)",
+                        CATALOG_NAME);
+        try (CloseableIterator<Row> rows = tEnv.executeSql(rebalance).collect()) {
+            List<String> actual =
+                    CollectionUtil.iteratorToList(rows).stream()
+                            .map(Row::toString)
+                            .collect(Collectors.toList());
+            assertThat(actual.size()).isGreaterThan(1);
+        }
+
+        // test cancel rebalance.
+        try (CloseableIterator<Row> listProceduresIterator =
+                tEnv.executeSql(String.format("Call %s.sys.cancel_rebalance()", CATALOG_NAME))
+                        .collect()) {
+            assertCallResult(listProceduresIterator, new String[] {"+I[success]"});
+        }
     }
 
     @Test
@@ -283,5 +382,17 @@ public abstract class FlinkProcedureITCase {
         conf.set(ConfigOptions.SUPER_USERS, "User:root");
         conf.set(ConfigOptions.AUTHORIZER_ENABLED, true);
         return conf;
+    }
+
+    private static void assertCallResult(CloseableIterator<Row> rows, String[] expected) {
+        List<String> actual =
+                CollectionUtil.iteratorToList(rows).stream()
+                        .map(Row::toString)
+                        .collect(Collectors.toList());
+        if (expected.length == 0) {
+            assertThat(actual).isEmpty();
+        } else {
+            assertThat(actual).containsExactlyInAnyOrder(expected);
+        }
     }
 }
