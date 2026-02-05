e9a9acf84eb6873e0c5aea52897762a311caeaf9
Liebing <liebingyu@gmail.com>
2026-02-05 11:58:07 +0800

    Revert "[paimon] Exclude paimon-bundle jar from fluss-lake-paimon to avoid class conflict (#2531)"
    This reverts commit 73d804205318d940bea830f3d93ec0ed5287f1b9.

--- website/src/plugins/remark-version-replace/index.js
+++ website/src/plugins/remark-version-replace/index.js
@@ -53,9 +53,7 @@ const plugin = (options) => {
         const replacements = {
             "$FLUSS_VERSION$": version.fullVersion,
             "$FLUSS_VERSION_SHORT$": version.shortVersion,
-            "$FLUSS_DOCKER_VERSION$": version.dockerVersion,
-            "$PAIMON_VERSION$": version.paimonVersion,
-            "$PAIMON_VERSION_SHORT$": version.paimonVersionShort
+            "$FLUSS_DOCKER_VERSION$": version.dockerVersion
         };
 
         // RegExp to find any replacement keys.
