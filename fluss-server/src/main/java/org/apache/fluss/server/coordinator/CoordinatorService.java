f3e1666788aedf13025794f97de9344177b09430
Liebing <liebingyu@gmail.com>
2026-03-11 17:16:50 +0800

    fix

--- fluss-server/src/main/java/org/apache/fluss/server/coordinator/CoordinatorService.java
+++ fluss-server/src/main/java/org/apache/fluss/server/coordinator/CoordinatorService.java
@@ -234,6 +234,7 @@ public final class CoordinatorService extends RpcServiceBase implements Coordina
     private final LakeTableHelper lakeTableHelper;
     private final ProducerOffsetsManager producerOffsetsManager;
     private final KvSnapshotLeaseManager kvSnapshotLeaseManager;
+    private final RemoteDirDynamicLoader remoteDirDynamicLoader;
 
     public CoordinatorService(
             Configuration conf,
@@ -256,7 +257,6 @@ public final class CoordinatorService extends RpcServiceBase implements Coordina
                 metadataManager,
                 authorizer,
                 dynamicConfigManager,
-                remoteDirDynamicLoader,
                 ioExecutor);
         this.defaultBucketNumber = conf.getInt(ConfigOptions.DEFAULT_BUCKET_NUMBER);
         this.defaultReplicationFactor = conf.getInt(ConfigOptions.DEFAULT_REPLICATION_FACTOR);
@@ -277,6 +277,7 @@ public final class CoordinatorService extends RpcServiceBase implements Coordina
         this.producerOffsetsManager.start();
 
         this.kvSnapshotLeaseManager = kvSnapshotLeaseManager;
+        this.remoteDirDynamicLoader = remoteDirDynamicLoader;
     }
 
     @Override
